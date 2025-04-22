9c397fa1076d4f4331957d4e00c1f703f1605515
Shlesh <mail2shleshitha@gmail.com>
2025-04-22 11:17:07 +0530

    cpp code removed

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -8,12 +8,9 @@ struct ListNode {
 };
 
 class Solution {
-    public:
-        void deleteNode(ListNode* node) {
-            node->val = node->next->val;
-            ListNode* temp = node->next;
-            node->next = node->next->next;
-            delete temp;
-        }
-    };
-    
+public:
+    void deleteNode(ListNode* node) {
+        // implement your code
+        
+    }
+};
